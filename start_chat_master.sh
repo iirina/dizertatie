901eea487172b162307dc2314481315780055771
Irina <iirina.iancu@gmail.com>
2016-06-21 11:48:49 +0200

    Added temporary cookie for master and slave nodes

--- start_chat_master.sh
+++ start_chat_master.sh
@@ -2,4 +2,4 @@
 cd chat_master/
 erl -make
 cd ..
-sudo erl -name 'chat_master@192.168.178.28' -env ERL_LIBS "." -eval "application:start(p1_mysql), application:start(chat_master)."
+sudo erl -name 'chat_master@192.168.178.28' -setcookie chatcookie -env ERL_LIBS "." -eval "application:start(p1_mysql), application:start(chat_master)."
--- start_chat_slave.sh
+++ start_chat_slave.sh
@@ -2,4 +2,4 @@
 cd chat_slave/
 erl -make
 cd ..
-sudo erl -name 'chat_slave@192.168.178.28' -env ERL_LIBS "."
+sudo erl -name 'chat_slave@192.168.178.28' -setcookie chatcookie -env ERL_LIBS "."
